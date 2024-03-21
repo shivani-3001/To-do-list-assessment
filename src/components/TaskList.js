cc7bff7f5f44a80cb15234650070c1a4f1bfe4e8
shivani-3001 <162289198+shivani-3001@users.noreply.github.com>
2024-03-21 21:00:03 +0530

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,6 @@ import { ListGroup } from 'react-bootstrap';
 import TaskItem from './TaskItem';
 
 const TaskList = ({ tasks, deleteTask, toggleTaskCompletion, editTask }) => {
-  console.log('tasksss=======', tasks)
   return (
     <ListGroup className="list-box">
       {tasks.map(task => (
